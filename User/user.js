74cb6268689e891544862d49eb04aa17428978ef
David Castillo Mora <davidcm2803@gmail.com>
2024-12-21 00:38:04 -0600

    user mejorado con logout

--- User/user.js
+++ User/user.js
@@ -1,20 +1,19 @@
 import { db, auth } from '../firebaseConfig.js';
 import { collection, getDocs } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js';
-import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js'; // IMPORTAR AQUÍ
+import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js';
+import { signOut } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js';
 
 // Escuchar el estado de autenticación
 onAuthStateChanged(auth, (user) => {
     const userDropdownButton = document.getElementById("dropdownUser");
 
     if (user) {
-        // Si el usuario está autenticado, mostrar su nombre
         const userName = user.displayName || user.email.split('@')[0];
         userDropdownButton.innerHTML = `<i class="bi bi-person-circle"></i> ${userName}`;
 
-        // Llamar a la función para cargar el historial de compras
+
         cargarHistorialDeCompras(user.uid);
     } else {
-        // Si no hay usuario autenticado, mostrar "User"
         userDropdownButton.innerHTML = `<i class="bi bi-person-circle"></i> User`;
     }
 });
@@ -29,7 +28,7 @@ async function cargarHistorialDeCompras(userId) {
         if (querySnapshot.empty) {
             historialElement.innerHTML = "<p>No tienes compras registradas.</p>";
         } else {
-            historialElement.innerHTML = '';  // Limpiar contenido anterior
+            historialElement.innerHTML = '';
 
             querySnapshot.forEach((doc) => {
                 const data = doc.data();
