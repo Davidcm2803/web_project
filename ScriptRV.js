0b3b18d4f8f071c915ea818e28b10928576d6c99
David Castillo Mora <davidcm2803@gmail.com>
2024-12-21 14:57:26 -0600

    lol

--- ScriptRV.js
+++ ScriptRV.js
@@ -30,42 +30,4 @@ reviewForm.addEventListener("submit", async (e) => {
   }
 });
 
-onSnapshot(collection(db, "reseñas"), (snapshot) => {
-  reviewsList.innerHTML = "";
 
-  const reviews = [];
-  snapshot.forEach((doc) => {
-    const review = doc.data();
-    reviews.push(review);
-  });
-
-  reviews.sort((a, b) => b.createdAt - a.createdAt);
-
-  const latestReviews = reviews.slice(0, 6);
-
-  latestReviews.forEach((review) => {
-    const slide = document.createElement("div");
-    slide.className = "swiper-slide";
-    slide.innerHTML = `
-          <div class="review-stars">${"★".repeat(review.stars)}</div>
-      <div class="review-name">${review.name}</div>
-      <div class="review-text">${review.text}</div>
-    `;
-    reviewsList.appendChild(slide);
-  });
-
-  new Swiper(".swiper", {
-    slidesPerView: "auto",
-    centeredSlides: true,
-    spaceBetween: 20,
-    loop: true,
-    autoplay: {
-      delay: 6000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-});
